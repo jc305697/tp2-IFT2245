871b6f15a75f21f54880a1ce62c3b43a048d4f2f
Unknown <jeremy.coulombe@hotmail.fr>
2018-03-12 14:57:40 -0400

    fichiers fournis
    fichiers fourni

---
+++ GNUmakefile
@@ -0,0 +1,69 @@
+CLIENT_DIR=client
+SERVER_DIR=server
+BUILD_DIR=build
+TIMEOUT=10
+VALGRIND=valgrind --leak-check=yes --error-exitcode=1
+CC=gcc
+CFLAGS=-g -std=c99 -Wall -pedantic -D_REENTRENT=1
+LDFLAGS=-pthread
+
+CLT_O = main.o client_thread.o
+SRV_O = main.o server_thread.o
+
+.PHONY: default all clean format client server test \
+	run \
+	run-client run-server \
+	run-client-valgrind run-server-valgrind
+
+default: all
+
+all: $(BUILD_DIR)/tp2_client $(BUILD_DIR)/tp2_server rapport.pdf
+
+$(BUILD_DIR)/tp2_client: $(patsubst %.o,$(BUILD_DIR)/client/%.o, $(CLT_O))
+	$(CC) $(LDFLAGS) -o $@ $(patsubst %.o,$(BUILD_DIR)/client/%.o, $(CLT_O))
+
+$(BUILD_DIR)/tp2_server: $(patsubst %.o,$(BUILD_DIR)/server/%.o, $(SRV_O))
+	$(CC) $(LDFLAGS) -o $@ $(patsubst %.o,$(BUILD_DIR)/server/%.o, $(SRV_O))
+
+$(BUILD_DIR)/%.o: %.c
+	@[ -d "$$(dirname "$@")" ] || mkdir -p "$$(dirname "$@")"
+	$(CC) $(CFLAGS) -c -o $@ $<
+
+
+
+# Lancer le client et le serveur, utilisant le port 2018 pour communiquer.
+# Ici on utilise par défaut 3 threads du côté du serveur et 5 du côté
+# du client.  Chaque client envoie 50 requêtes.  Il a 5 types de resources
+# à gérer avec quantités respectivement 10, 4, 23, 1, et 2.
+run: all
+	$(BUILD_DIR)/tp2_server 2018 3 & 		  \
+	$(BUILD_DIR)/tp2_client 2018 5 50   10 4 23 1 2 & \
+	wait
+
+run-server: all
+	@$(BUILD_DIR)/tp2_server 2018 3
+
+run-client: all
+	@$(BUILD_DIR)/tp2_client 2018 5 50   10 4 23 1 2
+
+run-server-valgrind: all
+	$(VALGRIND) $(BUILD_DIR)/tp2_server 2018
+
+run-client-valgrind: all
+	$(VALGRIND) $(BUILD_DIR)/tp2_client 2018 5 50   10 4 23 1 2 
+
+clean:
+	$(RM) -r $(BUILD_DIR) *.aux *.log
+
+%.pdf: %.tex
+	pdflatex -halt-on-error $<
+
+release:
+	tar -czv -f tp2.tar.gz --transform 's|^|tp2/|' \
+	    */*.[ch] *.tex *.md GNUmakefile
+
+
+$(BUILD_DIR)/server/main.o $(BUILD_DIR)/server/server_thread.o: \
+    server/server_thread.h
+$(BUILD_DIR)/client/main.o $(BUILD_DIR)/client/client_thread.o: \
+    client/client_thread.h
---
+++ server/server_thread.c
@@ -0,0 +1,218 @@
+#define _XOPEN_SOURCE 700   /* So as to allow use of `fdopen` and `getline`.  */
+
+#include "server_thread.h"
+
+#include <netinet/in.h>
+#include <netdb.h>
+
+#include <strings.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <signal.h>
+
+#include <time.h>
+
+enum { NUL = '\0' };
+
+enum {
+  /* Configuration constants.  */
+  max_wait_time = 30,
+  server_backlog_size = 5
+};
+
+unsigned int server_socket_fd;
+
+// Nombre de client enregistré.
+int nb_registered_clients;
+
+// Variable du journal.
+// Nombre de requêtes acceptées immédiatement (ACK envoyé en réponse à REQ).
+unsigned int count_accepted = 0;
+
+// Nombre de requêtes acceptées après un délai (ACK après REQ, mais retardé).
+unsigned int count_wait = 0;
+
+// Nombre de requête erronées (ERR envoyé en réponse à REQ).
+unsigned int count_invalid = 0;
+
+// Nombre de clients qui se sont terminés correctement
+// (ACK envoyé en réponse à CLO).
+unsigned int count_dispatched = 0;
+
+// Nombre total de requête (REQ) traités.
+unsigned int request_processed = 0;
+
+// Nombre de clients ayant envoyé le message CLO.
+unsigned int clients_ended = 0;
+
+// TODO: Ajouter vos structures de données partagées, ici.
+int *available;
+
+static void sigint_handler(int signum) {
+  // Code terminaison.
+  accepting_connections = 0;
+}
+
+void
+st_init ()
+{
+  // Handle interrupt
+  signal(SIGINT, &sigint_handler);
+
+  // Initialise le nombre de clients connecté.
+  nb_registered_clients = 0;
+
+  // TODO
+
+  // Attend la connection d'un client et initialise les structures pour
+  // l'algorithme du banquier.
+
+  // END TODO
+}
+
+void
+st_process_requests (server_thread * st, int socket_fd)
+{
+  // TODO: Remplacer le contenu de cette fonction
+  FILE *socket_r = fdopen (socket_fd, "r");
+  FILE *socket_w = fdopen (socket_fd, "w");
+
+  while (true)
+  {
+    char cmd[4] = {NUL, NUL, NUL, NUL};
+    if (!fread (cmd, 3, 1, socket_r))
+      break;
+    char *args = NULL; size_t args_len = 0;
+    ssize_t cnt = getline (&args, &args_len, socket_r);
+    if (!args || cnt < 1 || args[cnt - 1] != '\n')
+    {
+      printf ("Thread %d received incomplete cmd=%s!\n", st->id, cmd);
+      break;
+    }
+
+    printf ("Thread %d received the command: %s%s", st->id, cmd, args);
+
+    fprintf (socket_w, "ERR Unknown command\n");
+    free (args);
+  }
+
+  fclose (socket_r);
+  fclose (socket_w);
+  // TODO end
+}
+
+
+void
+st_signal ()
+{
+  // TODO: Remplacer le contenu de cette fonction
+
+
+
+  // TODO end
+}
+
+int st_wait() {
+  struct sockaddr_in thread_addr;
+  socklen_t socket_len = sizeof (thread_addr);
+  int thread_socket_fd = -1;
+  int end_time = time (NULL) + max_wait_time;
+
+  while(thread_socket_fd < 0 && accepting_connections) {
+    thread_socket_fd = accept(server_socket_fd,
+        (struct sockaddr *)&thread_addr,
+        &socket_len);
+    if (time(NULL) >= end_time) {
+      break;
+    }
+  }
+  return thread_socket_fd;
+}
+
+void *
+st_code (void *param)
+{
+  server_thread *st = (server_thread *) param;
+
+  int thread_socket_fd = -1;
+
+  // Boucle de traitement des requêtes.
+  while (accepting_connections)
+  {
+    // Wait for a I/O socket.
+    thread_socket_fd = st_wait();
+    if (thread_socket_fd < 0)
+    {
+      fprintf (stderr, "Time out on thread %d.\n", st->id);
+      continue;
+    }
+
+    if (thread_socket_fd > 0)
+    {
+      st_process_requests (st, thread_socket_fd);
+      close (thread_socket_fd);
+    }
+  }
+  return NULL;
+}
+
+
+//
+// Ouvre un socket pour le serveur.
+//
+void
+st_open_socket (int port_number)
+{
+  server_socket_fd = socket (AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+  if (server_socket_fd < 0)
+    perror ("ERROR opening socket");
+
+  if (setsockopt(server_socket_fd, SOL_SOCKET, SO_REUSEPORT, &(int){ 1 }, sizeof(int)) < 0) {
+    perror("setsockopt()");
+    exit(1);
+  }
+
+  struct sockaddr_in serv_addr;
+  memset (&serv_addr, 0, sizeof (serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons (port_number);
+
+  if (bind
+      (server_socket_fd, (struct sockaddr *) &serv_addr,
+       sizeof (serv_addr)) < 0)
+    perror ("ERROR on binding");
+
+  listen (server_socket_fd, server_backlog_size);
+}
+
+
+//
+// Affiche les données recueillies lors de l'exécution du
+// serveur.
+// La branche else ne doit PAS être modifiée.
+//
+void
+st_print_results (FILE * fd, bool verbose)
+{
+  if (fd == NULL) fd = stdout;
+  if (verbose)
+  {
+    fprintf (fd, "\n---- Résultat du serveur ----\n");
+    fprintf (fd, "Requêtes acceptées: %d\n", count_accepted);
+    fprintf (fd, "Requêtes : %d\n", count_wait);
+    fprintf (fd, "Requêtes invalides: %d\n", count_invalid);
+    fprintf (fd, "Clients : %d\n", count_dispatched);
+    fprintf (fd, "Requêtes traitées: %d\n", request_processed);
+  }
+  else
+  {
+    fprintf (fd, "%d %d %d %d %d\n", count_accepted, count_wait,
+        count_invalid, count_dispatched, request_processed);
+  }
+}
