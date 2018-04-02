b27fab45c5231e83e756da4f6a04394f738ca4c9
Maudie <whippou@gmail.com>
2018-04-02 15:48:14 -0400

    Added all functions

--- client/client_thread.h
+++ client/client_thread.h
@@ -10,7 +10,7 @@
 
 /* Port TCP sur lequel le serveur attend des connections.  */
 extern int port_number;
-//#include <string.h>
+
 /* Nombre de requêtes que chaque client doit envoyer.  */
 extern int num_request_per_client;
 
@@ -32,12 +32,16 @@ struct client_thread
   int *initmax;
 };
 
-int send_request (int client_id, int request_id, int socket_fd,char* message);
+
 bool send_config(int socket_fd);
 bool wait_answer(int socket_test);
 int make_random(int max_resources);
-void ct_start();
+int make_random_req(int max_resources);
+void lockIncrUnlock(pthread_mutex_t mymutex, int count);
+int send_request (int client_id, int request_id, int socket_fd,char* message);
+void make_request(client_thread* ct );
 
+void ct_start();
 void ct_open_socket();
 int client_connect_server();
 void ct_init (client_thread *);
